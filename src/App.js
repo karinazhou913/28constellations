e30b2b31bd1e2267f07cbe7676d2011a5ea6eaf3
yvette2333 <13071251886@163.com>
2025-03-19 23:12:40 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
+import Header from './components/Header';
+import IntroScreen from './components/IntroScreen';
+import Lottery from './components/Lottery';
+import Page1 from './components/Page1';
+// import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+import Page4 from './components/Page4';
 import './App.css';
 
 function App() {
+  const [tabIndex, setTabIndex] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        tabIndex === 0 ? null : <Header
+          tabIndex={tabIndex}
+          onChange={(index) => setTabIndex(index)}
+        />
+      }
+      {tabIndex === 1 ? <Lottery /> : null}
+      {
+        tabIndex === 0 ? <IntroScreen onChangeTab={(index) => setTabIndex(index)} /> : null
+      }
+      {
+        tabIndex === 1 ? <Page1 /> : null
+      }
+      {/* {
+        tabIndex === 2 ? <Page2 /> : null
+      }
+       */}
+      {
+        tabIndex === 3 ? <Page3 /> : null
+      }
+      {
+        tabIndex === 4 ? <Page4 /> : null
+      }
+
     </div>
   );
 }
